1d56b068969cdd1d6499ee7c6f59cbd9b12a7930
JHG-007 <jonathan100205@gmail.com>
2024-10-29 20:36:24 +0700

    class driver

--- Model/Class/User/Driver.java
+++ Model/Class/User/Driver.java
@@ -1,5 +1,60 @@
 package Model.Class.User;
 
+import java.util.Date;
+
 public class Driver {
-    
+    private String order;
+    private Date tglOrder;
+    private double rating;
+    private double pendapatan;
+    private String keluhan;
+
+    public Driver(String order, Date tglOrder, double rating, double pendapatan, String keluhan) {
+        this.order = order;
+        this.tglOrder = tglOrder;
+        this.rating = rating;
+        this.pendapatan = pendapatan;
+        this.keluhan = keluhan;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public Date getTglOrder() {
+        return tglOrder;
+    }
+
+    public void setTglOrder(Date tglOrder) {
+        this.tglOrder = tglOrder;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPendapatan() {
+        return pendapatan;
+    }
+
+    public void setPendapatan(double pendapatan) {
+        this.pendapatan = pendapatan;
+    }
+
+    public String getKeluhan() {
+        return keluhan;
+    }
+
+    public void setKeluhan(String keluhan) {
+        this.keluhan = keluhan;
+    }
+
 }
