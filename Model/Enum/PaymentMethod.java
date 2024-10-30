e33f61b9e1b8ace51d0b158517cf689da2c837ff
adrianrenardkristanto <kristantoadrian826@gmail.com>
2024-10-30 19:59:26 +0700

    class transaction

---
+++ Model/Enum/PaymentMethod.java
@@ -0,0 +1,5 @@
+package Model.Enum;
+
+public enum PaymentMethod {
+    CASH, OVO, CARD;
+}
---
+++ Model/Enum/StatusTransaction.java
@@ -0,0 +1,5 @@
+package Model.Enum;
+
+public enum StatusTransaction {
+    COMPLETED, CANCELLED, IN_PROGRESS;
+}
