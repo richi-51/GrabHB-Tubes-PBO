d91e3f531c4e4c9cdd3ccd07120b5bbd889b732c
JHG-007 <jonathan100205@gmail.com>
2024-10-30 16:31:53 +0700

    fix carseat to int

--- Model/Class/Vehicle/Car.java
+++ Model/Class/Vehicle/Car.java
@@ -1,20 +1,19 @@
 package Model.Class.Vehicle;
 
-public class Car extends Vehicle{
-    private String carSeat;
+public class Car extends Vehicle {
+    private int carSeat;
 
-    public Car(int driverID, String plateNumber, int vehicleID, String carSeat) {
+    public Car(int driverID, String plateNumber, int vehicleID, int carSeat) {
         super(driverID, plateNumber, vehicleID);
         this.carSeat = carSeat;
     }
 
-    public String getcarSeat() {
+    public int getCarSeat() {
         return carSeat;
     }
 
-    public void setcarSeat(String carSeat) {
+    public void setCarSeat(int carSeat) {
         this.carSeat = carSeat;
     }
 
-    
 }
