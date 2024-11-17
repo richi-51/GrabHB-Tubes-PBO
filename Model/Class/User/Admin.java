5d4c16b6632612d7a4fe0ed0015c6327876093c7
hp aza <richiewidjaya276@gmail.com>
2024-11-17 10:05:11 +0700

    Update enum, User, Add Db

--- Model/Class/User/Admin.java
+++ Model/Class/User/Admin.java
@@ -1,5 +1,25 @@
 package Model.Class.User;
 
+import java.util.Date;
+
+import Model.Enum.UserType;
+
 public class Admin extends User{
-    // lengkapi lagi
+    private int ID_admin;
+
+    // Constructor
+    public Admin(String username, String name, String password, String phoneNumber, String email, Date updateProfileAt, UserType userType, int id_admin){
+        super(username, name, password, phoneNumber, email, updateProfileAt, userType);
+        this.ID_admin = id_admin;
+    }
+
+    // Getter and Setter
+    public int getID_admin() {
+        return ID_admin;
+    }
+
+    public void setID_admin(int iD_admin) {
+        ID_admin = iD_admin;
+    }
+
 }
---
+++ Model/Enum/ServiceType.java
@@ -0,0 +1,5 @@
+package Model.Enum;
+
+public enum ServiceType {
+    GRABCAR, GRABBIKE;
+}
