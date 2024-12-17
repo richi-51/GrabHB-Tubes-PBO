9da10fce6d27f6bd558680ed020176ef44ed1195
JHG-007 <jonathan100205@gmail.com>
2024-12-17 21:17:33 +0700

    update customer order, report

--- src/Controller/CustomerController.java
+++ src/Controller/CustomerController.java
@@ -5,9 +5,13 @@ import java.util.Date;
 import Model.Class.Order.Order;
 import Model.Class.Payment.Ovo;
 import Model.Class.User.Customer;
+import Model.Enum.OrderStatus;
+import Model.Class.Order.Laporan;
 
 public class CustomerController {
     private Customer customer;
+    private Order order;
+    private Laporan keluhan;
 
     public CustomerController(Customer customer) {
         this.customer = customer;
@@ -17,37 +21,52 @@ public class CustomerController {
         if (customer.getOrder() == null) {
             return "Belum ada order";
         } else {
-           Order currentOrder = customer.getOrder();
-           return "Order " + currentOrder.getID_order() +
-               ", Pickup Location: " + currentOrder.getPickUpLoc() +
-                ", Destination: " + currentOrder.getDestination() +
-                ", Price: " + currentOrder.getPrice();
+           order = customer.getOrder();
+           return "Order " + order.getID_order() +
+               ", Pickup Location: " + order.getPickUpLoc() +
+                ", Destination: " + order.getDestination() +
+                ", Price: " + order.getPrice();
         }
     }
 
-    public void makeOrder(Order order) {
+    public void makeOrder(Order order) { // tinggal update order
         if (customer.getOrder() == null) {
             customer.setOrder(order);
+            order.setDestination(null);
+            order.setPickUpLoc(null);
+            order.setPaymentMethod(null);
+            order.setVoucher(null);
+            order.getServiceType();
         }
     }
 
     public void seeBalance(Ovo ovoE_money) {
+        customer.getID_Customer();
         customer.getOvoE_money();
     }
-
+    
     public void statusAcc(Date createdAccAt) {
+        customer.getID_Customer();
         customer.getCreatedAccAt();
     }
 
+    public void makeReport(){
+        // if (order.getOrder_status() == OrderStatus.COMPLETE) {
+        // }
+        order = customer.getOrder();
+        order.getKeluhan();
+        keluhan.setIsiKeluhan(null);  
+    }
+
     public void seeHistory(){
-        Order historyOrder = customer.getOrder();
-        historyOrder.getID_order();
-        historyOrder.getID_driver();
-        historyOrder.getPickUpLoc();
-        historyOrder.getDestination();
-        historyOrder.getOrder_date();
-        historyOrder.getOrder_status();
-        historyOrder.getPrice();
-        historyOrder.getServiceType();
+        order = customer.getOrder();
+        order.getID_order();
+        order.getID_driver();
+        order.getPickUpLoc();
+        order.getDestination();
+        order.getOrder_date();
+        order.getOrder_status();
+        order.getPrice();
+        order.getServiceType();
     }
 }
